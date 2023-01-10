001ae728fa65e73f78807c47cdf297f8b967837d
aracelliQA <aracelli.eng@gmail.com>
2023-01-10 11:09:32 -0300

    fourth release

---
+++ cypress/e2e/feedback.cy.js
@@ -0,0 +1,29 @@
+describe('Feedback Test', () => {
+    before(function() {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.get('#feedback').click()
+    })
+
+    it('Should load feedback form', () =>{
+        cy.url().should('include', 'feedback.html')
+        cy.get('h3').should('be.visible')
+
+    })
+
+    it('Should fill feedback form', () =>{
+        cy.get('#name').type('Ana')
+        cy.get('#email').type('ana@email.com')
+        cy.get('#subject').type('teste')
+        cy.get('#comment').type('teste teste teste')
+    })
+    
+   it('Should submit feedback form', () =>{
+    cy.contains('Send Message').click()
+        
+    })
+
+    it('Should display feedback message', () =>{
+    cy.get('.offset3').should('be.visible')
+        
+    })
+}) 
