6c32a74d069408b2ff0dd4bf56c202a74aee5187
aracelliQA <aracellieng@gmail.com>
2023-01-17 12:06:51 -0300

    sixth release

---
+++ cypress/e2e/new-payee.cy.js
@@ -0,0 +1,36 @@
+describe('Add new Payee', () => {
+  before (function (){
+    cy.visit('http://zero.webappsecurity.com/index.html')
+    cy.get('#signin_button').click()
+    cy.fixture('user').then(user =>{
+      const username = user.id
+      const password = user.psd
+      cy.get('#user_login').type(username)
+      cy.get('#user_password').type(password)
+      cy.get('#user_remember_me').click()
+      cy.contains('Sign in').click()
+    })
+    cy.contains('Pay Bills').click()
+    cy.contains('Add New Payee').click()
+  })
+
+  it('Should add new payee to the list', () => {
+    cy.get('#np_new_payee_name').type('test')
+    cy.get('#np_new_payee_address').type('adress')
+    cy.get('#np_new_payee_account').type('account')
+    cy.get('#np_new_payee_details').type('details')
+    cy.get('#add_new_payee').click()
+  })
+
+  it('Should show succsess message', () => {
+    cy.get('#alert_content').should('be.visible')
+
+  })
+
+  it('Should logout from application', () => {
+    cy.contains('username').click()
+    cy.get('#logout_link').click()
+    cy.url().should('include', 'index.html')
+})
+
+})
